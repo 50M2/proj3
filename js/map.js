5be515d2a1b2fea1d495ffbf72448750a32dc3ce
50M2 <toneyboyne@gmail.com>
2025-11-25 19:43:03 -0600

    incorrect cords

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 function initializeMap() {
-  const center_focus = { lat: 32.6858, lng: 117.1254};
+  const focus = { lat: 32.6858, lng: -117.1254};
 
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: center_focus,
+    center: focus,
     zoom: 14,
   });
 
   const marker = new google.maps.Marker({
-    position: center_focus,
+    position: focus,
     map: map,
     title: "Naval Base San Diego",
   });
