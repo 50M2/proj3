82db9b7a522e6953c3a63c3d41354e5476793ac2
50M2 <toneyboyne@gmail.com>
2025-11-25 19:35:41 -0600

    changed coords and hopefully fixed map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 function initializeMap() {
-  const center_focus = { lat: 22.3321, lng: 114.1903};
+  const center_focus = { lat: 32.6858, lng: 117.1254};
 
   const map = new google.maps.Map(document.getElementById("map"), {
     center: center_focus,
